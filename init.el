8ceddd63381fdff76051f9731e075c08b9979186
Vimal Earnest <v_earnest@yahoo.com>
2026-02-23 23:46:13 -0800

    Fix startup warnings for Emacs 30 compatibility
    - init.el: remove redundant package-initialize call (Emacs 27+ does it
      automatically before init file runs)
    - highlight-flash-conf.el: preceding-sexp → elisp--preceding-sexp
    - eval-sexp-fu submodule: defadvice → advice-add, cl aliases, lexical-binding
    - gist submodule: loop/every/some/lexical-let → cl-loop/cl-every/cl-some/let
    - org-pack: run make autoloads to generate org-loaddefs.el in org-mode/lisp/
      so org.el can find it relative to its own location

--- init.el
+++ init.el
@@ -4,11 +4,6 @@
 ;; It remembers you...
 
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
 
 (setq live-ascii-art-logo ";;
 ;;     MM\"\"\"\"\"\"\"\"`M
